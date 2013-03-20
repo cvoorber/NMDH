446198e70bbe272d0e05ce5a6a2288caf3e41a76
Ryan McColeman <ryan.mccoleman@gmail.com>
2013-03-20 19:40:16 -0400

    Applying changes from GH pull

---
+++ code/navigation/services.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class newsblog : System.Web.UI.Page
+{
+    newsClass objNews = new newsClass();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Master.pp_Url = "~/" + HttpContext.Current.Request.Url.AbsolutePath.ToString().Substring(6);
+    }
+
+    protected void Page_LoadComplete(object sender, EventArgs e)
+    {
+    }
+
+}
