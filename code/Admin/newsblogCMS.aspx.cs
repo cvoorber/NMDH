8e054f403e30ba36016884f0857d03f03812984f
chris <chris.voorberg@gmail.com>
2013-03-26 18:51:31 -0400

    Merged newsblog feature with Master, all functional

--- code/Admin/newsblogCMS.aspx.cs
+++ code/Admin/newsblogCMS.aspx.cs
@@ -1,14 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Globalization;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Admin_newsblogCMS : System.Web.UI.Page
 {
+    newsClass objLinq = new newsClass();
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+        {
+            _subRebind();
+        }
+    }
 
+    private void _subRebind()
+    {
+        txt_titleI.Text = string.Empty;
+        txt_descI.Text = string.Empty;
+        txt_imageI.Text = string.Empty;
+        txt_contactidI.Text = string.Empty;
+        txt_linkI.Text = string.Empty;
+
+        ddl_main.DataSource = objLinq.getNews();
+        ddl_main.DataTextField = "n_title";
+        ddl_main.DataValueField = "n_id";
+        ddl_main.DataBind();
+
+        int _id = int.Parse(ddl_main.SelectedValue.ToString());
+        lv_main.DataSource = objLinq.getNewsByID(_id);
+        lv_main.DataBind();
+    }
+
+    protected void subChange(object sender, EventArgs e)
+    {
+        int _id = int.Parse(ddl_main.SelectedValue.ToString());
+        lv_main.DataSource = objLinq.getNewsByID(_id);
+        lv_main.DataBind();
+    }
+
+    private void _strMessage(bool flag, string str)
+    {
+        if (flag)
+        {
+            lbl_message.Text = "<span style='color:green;'>News " + str + " was succesful.</span>";
+        }
+        else
+        {
+            lbl_message.Text = "<span style='color:red;'>Sorry, unable to " + str + " news.</span>";
+        }
+    }
+
+    protected void subInsert(object sender, EventArgs e)
+    {
+        string format = "dd/MM/yyyy hh:mm tt";
+        string stringDate = DateTime.Now.ToString(format, CultureInfo.InvariantCulture);
+        DateTime dateTime = DateTime.ParseExact(stringDate, format, CultureInfo.InvariantCulture);
+
+        _strMessage(objLinq.commitInsert(txt_titleI.Text, txt_descI.Text, txt_imageI.Text, dateTime, dateTime, int.Parse(txt_contactidI.Text.ToString()), txt_linkI.Text), "insert");
+        _subRebind();
+    }
+
+    protected void subAdmin(object sender, ListViewCommandEventArgs e)
+    {
+ 
+        switch (e.CommandName)
+        {
+            case "subUpdate":
+
+                TextBox txtTitle = (TextBox)e.Item.FindControl("txt_titleE");
+                TextBox txtDesc = (TextBox)e.Item.FindControl("txt_descE");
+                TextBox txtImage = (TextBox)e.Item.FindControl("txt_imageE");
+                TextBox txtContactID = (TextBox)e.Item.FindControl("txt_contactidE");
+                TextBox txtLink = (TextBox)e.Item.FindControl("txt_linkE");
+                HiddenField hdfID = (HiddenField)e.Item.FindControl("hdf_idE");
+                int newsID = int.Parse(hdfID.Value.ToString());
+
+                _strMessage(objLinq.commitUpdate(newsID, txtTitle.Text, txtDesc.Text, txtImage.Text, int.Parse(txtContactID.Text.ToString()), txtLink.Text), "update");
+                _subRebind();
+                break;
+
+            case "subDelete":
+                int _id = int.Parse(((HiddenField)e.Item.FindControl("hdf_idE")).Value);
+                _strMessage(objLinq.commitDelete(_id), "delete");
+                _subRebind();
+                break;
+        }
     }
 }
---
+++ code/newsblog.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class newsblog : System.Web.UI.Page
+{
+    newsClass objNews = new newsClass();
+
+    commentClass objComm = new commentClass();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            _subRebind();
+        }
+    }
+
+    protected void subChange(object sender, EventArgs e)
+    {
+        int _id = int.Parse(ddl_main.SelectedValue.ToString());
+        grd_main.DataSource = objNews.getNewsByID(_id);
+        grd_main.DataBind();
+
+        rpt_main.DataSource = objComm.getCommentsByID(_id);
+        rpt_main.DataBind();
+    }
+
+    private void _subRebind()
+    {
+        txt_nameI.Text = string.Empty;
+        txt_emailI.Text = string.Empty;
+        txt_commentI.Text = string.Empty;
+
+        ddl_main.DataSource = objNews.getNews();
+        ddl_main.DataTextField = "n_title";
+        ddl_main.DataValueField = "n_id";
+        ddl_main.DataBind();
+
+        int _id = int.Parse(ddl_main.SelectedValue.ToString());
+
+        grd_main.DataSource = objNews.getNewsByID(_id);
+        grd_main.DataBind();
+
+        rpt_main.DataSource = objComm.getCommentsByID(_id);
+        rpt_main.DataBind();
+    }
+
+    private void _strMessage(bool flag, string str)
+    {
+        if (flag)
+        {
+            lbl_message.Text = "<br /><span style='color:green;'>Comment " + str + " was succesful.</span>";
+        }
+        else
+        {
+            lbl_message.Text = "<br /><span style='color:red;'>Sorry, unable to " + str + " comment.</span>";
+        }
+    }
+
+    protected void subInsert(object sender, EventArgs e)
+    {
+        int artID = int.Parse(ddl_main.SelectedValue.ToString());
+        _strMessage(objComm.commitInsert(txt_nameI.Text, txt_emailI.Text, txt_commentI.Text, artID), "insert");
+        _subRebind();
+    }
+
+}
