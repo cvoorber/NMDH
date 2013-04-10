7d81c85a9a6c7c1dca641fb64453f09b2964403c
chris <chris.voorberg@gmail.com>
2013-04-09 20:49:51 -0400

    all CMS pages updated with new master page

--- code/Admin/newsblogCMS.aspx.cs
+++ code/Admin/newsblogCMS.aspx.cs
@@ -6,35 +6,53 @@ using System.Globalization;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 public partial class Admin_newsblogCMS : System.Web.UI.Page
 {
+    //instantiating the news class
     newsClass objLinq = new newsClass();
+
+    //instantiating the image class
+    imagesClass objImg = new imagesClass();
 
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!Page.IsPostBack)
         {
             _subRebind();
+            _Rebind();
         }
     }
 
+    //clear fields on refresh, bind drop downs from the news and uploads tables and Listview from the news table
     private void _subRebind()
     {
         txt_titleI.Text = string.Empty;
         txt_descI.Text = string.Empty;
-        txt_imageI.Text = string.Empty;
         txt_contactidI.Text = string.Empty;
-        txt_linkI.Text = string.Empty;
 
         ddl_main.DataSource = objLinq.getNews();
         ddl_main.DataTextField = "n_title";
         ddl_main.DataValueField = "n_id";
         ddl_main.DataBind();
 
+        ddl_image.DataSource = objImg.getImages();
+        ddl_image.DataTextField = "ImageName";
+        ddl_image.DataValueField = "ImageURL";
+        ddl_image.DataBind();
+
         int _id = int.Parse(ddl_main.SelectedValue.ToString());
         lv_main.DataSource = objLinq.getNewsByID(_id);
         lv_main.DataBind();
+    }
+
+    //binding the images from the database to the DataList
+    private void _Rebind()
+    {
+        imagesClass objImg = new imagesClass();
+        dtl_main.DataSource = objImg.getImages();
+        dtl_main.DataBind();
     }
 
     protected void subChange(object sender, EventArgs e)
@@ -62,7 +80,7 @@ public partial class Admin_newsblogCMS : System.Web.UI.Page
         string stringDate = DateTime.Now.ToString(format, CultureInfo.InvariantCulture);
         DateTime dateTime = DateTime.ParseExact(stringDate, format, CultureInfo.InvariantCulture);
 
-        _strMessage(objLinq.commitInsert(txt_titleI.Text, txt_descI.Text, txt_imageI.Text, dateTime, dateTime, int.Parse(txt_contactidI.Text.ToString()), txt_linkI.Text), "insert");
+        _strMessage(objLinq.commitInsert(txt_titleI.Text, txt_descI.Text, ddl_image.SelectedValue.ToString(), dateTime, dateTime, int.Parse(txt_contactidI.Text.ToString())), "insert");
         _subRebind();
     }
 
@@ -77,11 +95,10 @@ public partial class Admin_newsblogCMS : System.Web.UI.Page
                 TextBox txtDesc = (TextBox)e.Item.FindControl("txt_descE");
                 TextBox txtImage = (TextBox)e.Item.FindControl("txt_imageE");
                 TextBox txtContactID = (TextBox)e.Item.FindControl("txt_contactidE");
-                TextBox txtLink = (TextBox)e.Item.FindControl("txt_linkE");
                 HiddenField hdfID = (HiddenField)e.Item.FindControl("hdf_idE");
                 int newsID = int.Parse(hdfID.Value.ToString());
 
-                _strMessage(objLinq.commitUpdate(newsID, txtTitle.Text, txtDesc.Text, txtImage.Text, int.Parse(txtContactID.Text.ToString()), txtLink.Text), "update");
+                _strMessage(objLinq.commitUpdate(newsID, txtTitle.Text, txtDesc.Text, txtImage.Text, int.Parse(txtContactID.Text.ToString())), "update");
                 _subRebind();
                 break;
 
@@ -90,6 +107,51 @@ public partial class Admin_newsblogCMS : System.Web.UI.Page
                 _strMessage(objLinq.commitDelete(_id), "delete");
                 _subRebind();
                 break;
+        }
+    }
+
+    //file upload
+    protected void subImage(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            Boolean fileOK = false;
+            String path = Server.MapPath("~/img/");
+            if (FileUploader.HasFile)
+            {
+                String fileExtension =
+                    Path.GetExtension(FileUploader.FileName).ToLower();
+                String[] allowedExtensions = { ".gif", ".png", ".jpg", ".jpeg" };
+                for (int i = 0; i < allowedExtensions.Length; i++)
+                {
+                    if (fileExtension == allowedExtensions[i])
+                    {
+                        fileOK = true;
+                    }
+                }
+            }
+
+            if (fileOK)
+            {
+                try
+                {
+                    FileUploader.PostedFile.SaveAs(path + FileUploader.FileName);
+                    string ImgURL = System.IO.Path.GetFileName(FileUploader.FileName);
+                    TextBox txtNameI = (TextBox)FileUploader.FindControl("txt_imagename");
+                    imagesClass objImgI = new imagesClass();
+                    objImgI.ImageName = txtNameI.Text.ToString();
+                    objImgI.ImageURL = "~/img/" + ImgURL.ToString();
+                    lbl_msg.Text = objImgI.insertImage();
+                }
+                catch (Exception ex)
+                {
+                    lbl_msg.Text = "<span style='color:red;'>File could not be uploaded</span> -- " + ex.Message.ToString();
+                }
+            }
+            else
+            {
+                lbl_msg.Text = "<span style='color:orange;'>Only .gif, .png, .jpg or .jpeg files can be uploaded</span>";
+            }
         }
     }
 }
