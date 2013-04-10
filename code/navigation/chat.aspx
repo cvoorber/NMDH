b703dc2d7f88150e4a8480b0d5262d63bced0fd7
DietCokeFiend <quinyung@gmail.com>
2013-04-10 15:44:48 -0400

    commented chat page

--- code/navigation/chat.aspx
+++ code/navigation/chat.aspx
@@ -4,6 +4,7 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="r_content" Runat="Server">
 
+    <%-- script to make scrollbar on chatwindow scroll down to newest msg on rebind --%>
     <script type="text/javascript">
         function divScroll() {
             var targetDiv = document.getElementById("chatbox");
@@ -11,25 +12,31 @@
         }
     </script>
     
+    <%--Scriptmanager to handle all ajax functionality --%>
     <asp:ScriptManager ID="scm_main" runat="server" />
     <asp:Label ID="lbl_status" runat="server" />
     <br /><br />
-    <asp:Panel ID="pnl_status" runat="server">
 
+    <%-- initial form for user to enter nickname --%>
+    <asp:Panel ID="pnl_status" runat="server">
         <asp:Label ID="lbl_nickname" runat="server" Text="Enter a nickname:" />
         <br />
         <asp:TextBox ID="txt_nickname" runat="server" />
+        <asp:RequiredFieldValidator ID="rfv_nickname" runat="server" ControlToValidate="txt_nickname" ErrorMessage="*required" />
        
         <asp:Button ID="btn_submit" runat="server" Text="Submit" OnClick="subSubmit" />
     </asp:Panel>
 
+
+    <%-- common message area where messages are shown and updated --%>
     <asp:Panel ID="pnl_chat" runat="server">
         <asp:Label ID="lbl_nicknameC" runat="server" />
         <br /><br />
-        
 
         <asp:Label ID="lbl" runat="server" Text="Username: Quin" />
         <div id="chatbox" style="width:580px;border:1px solid black;height:300px; overflow-y:scroll;">
+            
+            <%-- update panel for asynchronously update common message area --%>
             <asp:UpdatePanel ID="upl_main" runat="server" UpdateMode="Conditional">
                 <ContentTemplate>
                      <asp:Repeater ID="rpt_chat" runat="server">
@@ -42,12 +49,15 @@
                     </asp:Repeater>
                 </ContentTemplate>
                 <Triggers>
+                    <%-- refresh is triggered by send button or timer --%>
                     <asp:AsyncPostBackTrigger ControlID="btn_send" EventName="Click" />
                     <asp:AsyncPostBackTrigger ControlID="tmr_main" EventName="Tick" />
                 </Triggers>
             </asp:UpdatePanel>
             <asp:Timer ID="tmr_main" runat="server" OnTick="subRefresh" Interval="2000" />
         </div>
+
+        <%-- message textbox is ajax'd also to ensure it is cleared upon send button click --%>
         <asp:UpdatePanel ID="upl_msgbox" runat="server" UpdateMode="Conditional">
             <ContentTemplate>
                 <asp:TextBox ID="txt_msg" runat="server" TextMode="MultiLine" style="width:500px; height:75px; max-width:500px; max-height:75px;float:left;" />
--- code/navigation/chat.aspx.cs
+++ code/navigation/chat.aspx.cs
@@ -12,23 +12,25 @@ public partial class _Default : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        
-
         if (!Page.IsPostBack)
         {
+            //if nickname is in query string from postback
             if (Request.QueryString["nickname"] != null)
             {
+                //put nickname in session variable
                 Session["nickname"] = Request.QueryString["nickname"].ToString();
 
+                //if no chatroom/chatID assigned to session
+                //get next available
                 if(Session["chatroom"] == null)
-                    Session["chatroom"] = getChatRoom(); 
+                    Session["chatroom"] = _getChatRoom(); 
                 
-                showPanel(pnl_chat);
-                rebind();
+                _showPanel(pnl_chat);
+                _rebind();
             }
             else
             {
-                showPanel(pnl_status);
+                _showPanel(pnl_status);
             }
            ;
         }
@@ -40,6 +42,7 @@ public partial class _Default : System.Web.UI.Page
         {
             ndmh_chat newChatObj = new ndmh_chat()
             {
+                //convert newline characters to br tags before sending to database
                 message = txt_msg.Text.Replace("\n", "<br />"),
                 chatroomID = Int32.Parse(Session["chatroom"].ToString()),
                 timestamp = DateTime.Now,
@@ -47,17 +50,17 @@ public partial class _Default : System.Web.UI.Page
             };
             
             chatDBObj.Insert(newChatObj);
-            rebind();
+            _rebind();
         }
         txt_msg.Text = "";
     }
 
     protected void subRefresh(object sender, EventArgs e)
     {
-        rebind();
+        _rebind();
     }
 
-    private void rebind()
+    private void _rebind()
     {
         rpt_chat.DataSource = chatDBObj.getResultByColumn(m => m.chatroomID == Int32.Parse(Session["chatroom"].ToString()))
             .OrderBy(m => m.timestamp);
@@ -68,29 +71,36 @@ public partial class _Default : System.Web.UI.Page
         ScriptManager.RegisterStartupScript(this.Page, typeof(Page), "scrolltobottom", ";divScroll();", true);
     }
 
-    private void showPanel(Panel p)
+    private void _showPanel(Panel p)
     {
         pnl_chat.Visible = false;
         pnl_status.Visible = false;
         p.Visible = true;
     }
 
-
-    private int getChatRoom()
+    //get first available chatID/chatroom
+    private int _getChatRoom()
     {
         int chID = 0;
+        
+        //if count is 0, there are no admin on to create 'chatrooms'
         if (ChatClass.chatrooms.Count == 0)
         {
-            disableChat();
+            _disableChat();
             lbl_status.Text = "Chat is Offline.";
         }
         else
         {
+            //loop through chatrooms to see what is available
+            //if chatroom is available (denoted by not having username set
+            //and admin is set
             for (int i = 0; i < ChatClass.chatrooms.Count; i++)
             {
                 if (ChatClass.chatrooms[i].userName == "" && ChatClass.chatrooms[i].adminName != "")
                 {
                     ChatClass.chatrooms[i].userName = Session["nickname"].ToString();
+                    
+                    //chatrooms / chatID starts from 1 even though array/list starts from index zero
                     chID = i + 1;
                 }
                 else
@@ -100,13 +110,17 @@ public partial class _Default : System.Web.UI.Page
             }
           
         }
+
+        //return first available
         return chID;
     }
 
-    private void disableChat()
+    private void _disableChat()
     {
     }
 
+    //when nickname form is submitted 
+    //postback to page with nickname
     protected void subSubmit(object sender, EventArgs e)
     {
         Response.Redirect("chat.aspx?nickname="+txt_nickname.Text);
