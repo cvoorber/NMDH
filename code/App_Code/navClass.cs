ca9c5f63ad7e54731b9e73b0923b5dd4241edb40
Ilana Beer <IlanaMBeer@gmail.com>
2013-04-10 01:50:43 -0400

    yay more comments

--- code/App_Code/navClass.cs
+++ code/App_Code/navClass.cs
@@ -6,6 +6,7 @@ using System.Data.Linq;
 
 public class navClass
 {
+    //this method returns all the pages from the general pages table
     public IQueryable<ndmh_general_page> getAll()
     {
         ndmhDCDataContext objPageDC = new ndmhDCDataContext();
@@ -13,6 +14,7 @@ public class navClass
         return allPage;
     }
 
+    //this method returns one page based on the name(title) provided
     public IQueryable<ndmh_general_page> getPageByName(string _name)
     {
         ndmhDCDataContext objPageDC = new ndmhDCDataContext();
@@ -20,6 +22,7 @@ public class navClass
         return allPage;
     }
 
+    //return the record matching the provided id
     public IQueryable<ndmh_general_page> getPageByID(int _id)
     {
         ndmhDCDataContext objPageDC = new ndmhDCDataContext();
@@ -27,6 +30,7 @@ public class navClass
         return allPage;
     }
 
+    //add a new page to the database
     public bool newPage(string _title, string _type, string _content, string _image, bool _publish)
     {
         ndmhDCDataContext objPageDC = new ndmhDCDataContext();
@@ -49,6 +53,7 @@ public class navClass
         }
     }
 
+    //update an existing record
     public bool updatePage(int _id, string _title, string _type, string _content, bool _publish)
     {
         ndmhDCDataContext objPageDC = new ndmhDCDataContext();
@@ -69,6 +74,7 @@ public class navClass
         }
     }
 
+    //delete a record based on the id provided
     public bool deletePage(int _id)
     {
         ndmhDCDataContext objPageDC = new ndmhDCDataContext();
--- code/App_Code/staffroomClass.cs
+++ code/App_Code/staffroomClass.cs
@@ -9,6 +9,7 @@ using System.Data.Linq;
 /// </summary>
 public class staffroomClass
 {
+    //return all calendar events
     public IQueryable<ndmh_roomBooking> getCalendar()
     {
         ndmhDCDataContext objCalendarDC = new ndmhDCDataContext();
@@ -16,16 +17,22 @@ public class staffroomClass
         return allDates;
     }
 
+    //return all events that fit within a certain date range (one day and the day after to provide a timeslot of 24 hours that an event can fit into)
     public IQueryable<ndmh_roomBooking> getEventsbyDay(DateTime _day, DateTime _nextday)
     {
         ndmhDCDataContext objCalendarDC = new ndmhDCDataContext();
         DataLoadOptions options = new DataLoadOptions();
+
+        //join with the staff listing table to get member name and contact info
         options.LoadWith<ndmh_roomBooking>(x => x.ndmh_staff_listing);
         objCalendarDC.LoadOptions = options;
+
+        //execute the query and return the records
         var allDates = objCalendarDC.ndmh_roomBookings.Where(x => x.rb_start > _day && x.rb_start < _nextday).Select(x => x).OrderBy(x => x.rb_start);
         return allDates;
     }
 
+    //return the event matching the id provided
     public IQueryable<ndmh_roomBooking> getEventbyID(int _id)
     {
         ndmhDCDataContext objCalendarDC = new ndmhDCDataContext();
