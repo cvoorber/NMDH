e9a78eea58058abd2d6ffe6816e0192263688659
Ilana Beer <IlanaMBeer@gmail.com>
2013-04-10 09:31:23 -0400

    weee comments

--- code/SubMaster1.master.cs
+++ code/SubMaster1.master.cs
@@ -9,6 +9,7 @@ using System.Xml;
 
 public partial class SubMaster1 : System.Web.UI.MasterPage
 {
+    //expose the url publicly so that the page can pass through its url
     private string _url = "";
     public string pp_Url
     {
@@ -18,43 +19,66 @@ public partial class SubMaster1 : System.Web.UI.MasterPage
 
     protected void Page_Load(object sender, EventArgs e)
     {
+        //fill the side menu
         _loadMenu();
+
+        //create an event for when a menu item is clicked
         side_menu.MenuItemClick += sideMenu_Click;
+
+        //load the initial content on the page
         if (!Page.IsPostBack)
         {
+            //locate the sitemap
             string xmlPath = Request.PhysicalApplicationPath + @"XMLSitemap.xml";
+            //create a new XmlDocument object
             XmlDocument doc = new XmlDocument();
+            //load the sitemap into the document
             doc.Load(xmlPath);
+            //load the url nodes into an XML nodelist
             XmlNodeList titleList = doc.GetElementsByTagName("url");
+            //declare the string pagename
             string pagename = "";
+
+            //loop through each node in titlelist (list of page titles)
             for (var i = 0; i < titleList.Count; i++)
             {
+                //check if the node's url matches that of the current page
                 if(titleList[i].ChildNodes[0].FirstChild.Value == pp_Url)
                 {
+                    //set the pagename variable to the node that matches the current page
+                    //this will be used later to retrieve the content
                     pagename = titleList[i].ChildNodes[2].FirstChild.Value.ToString();
                     break;
                 }
             }
+            //create a new instance of the navigation class
             navClass menuObj = new navClass();
+            //bind the repeater to the content of that page
             rpt_content.DataSource = menuObj.getPageByName(pagename);
             rpt_content.DataBind();
         }
     }
 
+    //method to handle the change of content on the page (when the sidemenu is clicked)
     protected void sideMenu_Click(object sender, MenuEventArgs e)
     {
+        //ensure that the menu item has a value
         if (e.Item.Value != null)
         {
+            //create a new instance of the navClass and bind the content to that requested
             navClass menuObj = new navClass();
             rpt_content.DataSource = menuObj.getPageByName(e.Item.Value);
             rpt_content.DataBind();
         }
     }
 
+    //method to load the submenu
     public void _loadMenu()
     {
+        //clear the current list of menuitems (items may have changed)
         side_menu.Items.Clear();
 
+        //locate the xml document
         string xmlPath = Request.PhysicalApplicationPath + @"XMLSitemap.xml";
         XmlDocument doc = new XmlDocument();
         doc.Load(xmlPath);
