0b5957f3bc56420f68428595fe83926c3ac3d489
Ilana Beer <IlanaMBeer@gmail.com>
2013-04-10 10:11:22 -0400

    woo more comments!

--- code/SubMaster1.master.cs
+++ code/SubMaster1.master.cs
@@ -80,22 +80,30 @@ public partial class SubMaster1 : System.Web.UI.MasterPage
 
         //locate the xml document
         string xmlPath = Request.PhysicalApplicationPath + @"XMLSitemap.xml";
+        //create a new XmlDocument object to store the xml file
         XmlDocument doc = new XmlDocument();
         doc.Load(xmlPath);
+        //load a list of url nodes
         XmlNodeList titleList = doc.GetElementsByTagName("url");
+        //declare a string that represents the parent of the current url
         string urlparent = "";
+        //loop through each url node
         foreach (XmlNode node in titleList)
         {
+            //if the node's url matches the current page url, set the parent to that node's parent tag value
             if (node.ChildNodes[0].FirstChild.Value == pp_Url)
             {
                 urlparent = node.ChildNodes[3].FirstChild.Value.ToString();
             }
         }
 
+        //loop through the titlelist
         foreach (XmlNode node in titleList)
         {
+            //if the node's parent matches the parent string value determined above (the parent is the page requested)
             if (node.ChildNodes[3].FirstChild.Value == urlparent)
             {
+                //create a new MenuItem that stores the node
                 MenuItem output = new MenuItem();
                 output.Text = node.ChildNodes[2].FirstChild.Value;
                 output.Target = node.ChildNodes[0].FirstChild.Value.ToString();
@@ -105,6 +113,7 @@ public partial class SubMaster1 : System.Web.UI.MasterPage
         }
     }
 
+    // initiate the eventhandler passed down from the masterpage
     protected override void OnInit(EventArgs e)
     {
         base.OnInit(e);
@@ -112,27 +121,40 @@ public partial class SubMaster1 : System.Web.UI.MasterPage
         ((maintemplate)this.Master).downFont += new EventHandler(fontDec);
     }
 
+    //increase the font size of the repeater on the page
     protected void fontInc(object sender, EventArgs e)
     {
+        //check if the repeater is populated
         if (rpt_content.Items.Count > 0)
         {
+            // create new title and content objects
             Label title = new Label();
             Label content = new Label();
+
+            //loop through each item in the repeater (which should be one since it matches an ID)
+            //this is used to access the controls in the repeater
             foreach (RepeaterItem ri in rpt_content.Items)
             {
+                //set the title label to the matching control in the repater
                 title = (Label)ri.FindControl("lbltitle");
                 content = (Label)ri.FindControl("lblcontent");
             }
+
+            //increase the font size of each by 2pt
             int newsizeSide = int.Parse(side_menu.Font.Size.Unit.ToString().Substring(0, 2)) + 2;
             int newsizeHead = int.Parse(title.Font.Size.Unit.ToString().Substring(0, 2)) + 2;
             int newsizeContent = int.Parse(content.Font.Size.Unit.ToString().Substring(0, 2)) + 2;
 
+            //the maximum size of the side menu is 16pt
             if (newsizeSide > 16)
             {
+                //at this point, the new font size variable for each control is equal to its current size
                 newsizeSide = int.Parse(side_menu.Font.Size.Unit.ToString().Substring(0, 2));
                 newsizeHead = int.Parse(title.Font.Size.Unit.ToString().Substring(0, 2));
                 newsizeContent = int.Parse(content.Font.Size.Unit.ToString().Substring(0, 2));
             }
+
+            //set the new font size to match the determined variable
             side_menu.Font.Size = FontUnit.Point(newsizeSide);
             title.Font.Size = FontUnit.Point(newsizeHead);
             content.Font.Size = FontUnit.Point(newsizeContent);
@@ -141,26 +163,37 @@ public partial class SubMaster1 : System.Web.UI.MasterPage
 
     protected void fontDec(object sender, EventArgs e)
     {
+        //check if the repeater is populated
         if (rpt_content.Items.Count > 0)
         {
+            // create new title and content objects
             Label title = new Label();
             Label content = new Label();
+
+            //loop through each item in the repeater (which should be one since it matches an ID)
+            //this is used to access the controls in the repeater
             foreach (RepeaterItem ri in rpt_content.Items)
             {
+                //set the title label to the matching control in the repater
                 title = (Label)ri.FindControl("lbltitle");
                 content = (Label)ri.FindControl("lblcontent");
             }
 
+            //increase the font size of each by 2pt
             int newsizeSide = int.Parse(side_menu.Font.Size.Unit.ToString().Substring(0, 2)) - 2;
             int newsizeHead = int.Parse(title.Font.Size.Unit.ToString().Substring(0, 2)) - 2;
             int newsizeContent = int.Parse(content.Font.Size.Unit.ToString().Substring(0, 2)) - 2;
 
-            if (newsizeSide < 10)
+            //the minimum size of the side menu is 10pt
+            if (newsizeSide <10)
             {
+                //at this point, the new font size variable for each control is equal to its current size
                 newsizeSide = int.Parse(side_menu.Font.Size.Unit.ToString().Substring(0, 2));
                 newsizeHead = int.Parse(title.Font.Size.Unit.ToString().Substring(0, 2));
                 newsizeContent = int.Parse(content.Font.Size.Unit.ToString().Substring(0, 2));
             }
+
+            //set the new font size to match the determined variable
             side_menu.Font.Size = FontUnit.Point(newsizeSide);
             title.Font.Size = FontUnit.Point(newsizeHead);
             content.Font.Size = FontUnit.Point(newsizeContent);
